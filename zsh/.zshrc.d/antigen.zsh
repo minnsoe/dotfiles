8f530a2404ac0866115800dfec59d9c311036ec3
Minn Soe <contributions@minn.io>
2019-09-25 00:18:50 +0100

    Split antigen zsh config into own file and add confd style loader

---
+++ zsh/.zshrc.d/antigen.zsh
@@ -0,0 +1,20 @@
+source $HOME/.zsh/antigen/antigen.zsh
+
+antigen use oh-my-zsh
+antigen theme simple
+
+## Load macOS specific bundles
+if [[ $OSTYPE == darwin* ]]; then
+  antigen bundle osx
+fi
+
+## Batch load bundles
+antigen bundles <<EOBUNDLES
+  zsh-users/zsh-completions
+  zsh-users/zsh-syntax-highlighting
+  zsh-users/zsh-autosuggestions
+  git
+EOBUNDLES
+
+## Must be run after loading the bundles to finalise
+antigen apply
