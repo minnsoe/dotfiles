7d9a9931bc183decb379e0b02f3079dc9eac8de5
Minn Soe <contributions@minn.io>
2021-05-16 17:20:08 +0100

    Reduce zsh init time and fix fg color

--- dot_zshrc.d/zinit.zsh
+++ dot_zshrc.d/zinit.zsh
@@ -1,18 +1,23 @@
 if [ -f "$HOME/.zinit/bin/zinit.zsh" ]; then
     source "$HOME/.zinit/bin/zinit.zsh"
 
+    ZSH_AUTOSUGGEST_MANUAL_REBIND="true"
+    ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=0"
+
     zinit wait lucid for \
-        "Aloxaf/fzf-tab" \
         "OMZP::ssh-agent" \
         "OMZP::pyenv" \
         "OMZP::direnv" \
-        "OMZP::nvm"
-
-    zinit wait lucid for \
+        "OMZP::nvm" \
         atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
+            "Aloxaf/fzf-tab" \
             "zdharma/fast-syntax-highlighting" \
         blockf \
             "zsh-users/zsh-completions" \
         atload"!_zsh_autosuggest_start" \
             "zsh-users/zsh-autosuggestions"
+
+    autoload -Uz compinit
+    compinit
+    zinit cdreplay -q
 fi
